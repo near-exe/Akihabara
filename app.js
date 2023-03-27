1cd16dc41552808b47cd653d100f79c0f847d2ec
Near <ogmelgen@gmail.com>
2023-03-27 02:47:07 -0400

    New server and insert to db implementation

--- app.js
+++ app.js
@@ -1,68 +1,19 @@
-//Variables con los metodos de express/mysql/y el servidor https
+//Variables con los metodos de express y mysql
 const express = require('express');
 const app = express();
 const port = 8080;
 const mysql = require('mysql');
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-const path = require('path');
 
-//app.use(express.urlencoded({ extended: true }));
-
-
-//Creación del server que va a alojar los archivos que estan en el root folder
-const server = http.createServer((req, res) => {
-    const filePath = req.url === '/' ? './index.html' : `.${req.url}`;
-    const extname = path.extname(filePath);
-    let contentType = 'text/html';
-    switch (extname) {
-      case '.js':
-        contentType = 'text/javascript';
-        break;
-      case '.css':
-        contentType = 'text/css';
-        break;
-      case '.json':
-        contentType = 'application/json';
-        break;
-      case '.png':
-        contentType = 'image/png';
-        break;
-      case '.jpg':
-        contentType = 'image/jpg';
-        break;
-      case '.svg':
-        contentType = 'image/svg+xml';
-        break;
-    }
-    fs.readFile(filePath, (err, content) => {
-      if (err) {
-        if (err.code == 'ENOENT') {
-          res.writeHead(404);
-          res.end('404 Not Found');
-        } else {
-          res.writeHead(500);
-          res.end(`Error 500, problema con el servidor: ${err.code}`);
-        }
-      } else {
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(content, 'utf-8');
-      }
-    });
-  });
-
-//Prender la escucha en el puerto 3000
-server.listen(port, () => {
-  console.log(`El server esta corriendo en el puerto ${port}`);
-});
+//Servidor con todos los archivos del proyecto
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./'));
 
 //Variable con la conexion a la base de datos y sus metodos
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'persons'
+    database: 'testing'
   });
   
 
@@ -71,3 +22,24 @@ const connection = mysql.createConnection({
     if (err) throw err;
     console.log('Conexión con la base de datos exitosa');
   });
+
+  //Funcion que coge la data del login y realiza un query de insert a la base de datos
+  app.post('/submit-form', (req, res) => {
+    const name = req.body.name;
+    const id = req.body.id;
+  
+    const sql = `INSERT INTO persons (PersonID, FirstName) VALUES (?, ?)`;
+    const values = [id,name];
+  
+    connection.query(sql, values, (err, result) => {
+      if (err) throw err;
+      console.log('Data insertada en la base de datos');
+      res.send('Datos subidos de manera satisfactoria');
+    });
+  });
+
+  //Prender la escucha en el puerto 3000
+  app.listen(port, () => {
+    console.log(`El server esta corriendo en el puerto ${port}`);
+  });
+  
