353c68167c976cab9cced17d62f65a7ddab5678c
niko <niko.londonohernandez@gmail.com>
2023-04-01 23:45:29 -0400

    transiciones index y loader en el login

---
+++ js/login.js
@@ -0,0 +1,7 @@
+// loader
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+    loader.addEventListener("animationend", () => {
+      document.body.removeChild(loader);
+    });
+  });
