1e68041e7a7f13b51d4562f43caef75d9cdc4413
Near <ogmelgen@gmail.com>
2023-04-08 02:10:41 -0400

    Login UI and Admin dashboard fix implemented

--- js/login.js
+++ js/login.js
@@ -5,3 +5,56 @@ window.addEventListener("load", () => {
       document.body.removeChild(loader);
     });
   });
+
+
+  //Mensajes de feedback
+  const user = document.getElementById("user");
+  const password = document.getElementById("password");
+  const msg = document.getElementById("msg");
+
+  //sessionStorage.setItem("LoginMSG" , "");
+  msg.innerHTML = sessionStorage.getItem("LoginMSG");
+  localStorage.setItem("Logged", "False");
+
+
+  function Check()
+  {
+        //Traer la data de la ruta /logincheck que se manda desde el backend (Si se paso los checks del login)
+        fetch('/logincheck')
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          let flag = data;
+          if(flag)
+          {
+            return true;
+          }
+          else
+          {
+            return false;
+          }
+        });
+  }
+
+  function validateInput ()
+  {
+    let pass = password.value;
+    let user1 = user.value;
+    if(pass == "" || user1 == "")
+    {
+       sessionStorage.setItem("LoginMSG","Faltan completar campos");
+       msg.innerHTML = sessionStorage.getItem("LoginMSG");
+    }
+    else if(!Check())
+    {
+        sessionStorage.setItem("LoginMSG","Nombre de usuario o contraseña incorrectos");
+        msg.innerHTML = sessionStorage.getItem("LoginMSG");
+    }
+    else
+    {
+        sessionStorage.setItem("LoginMSG","");
+        msg.innerHTML = sessionStorage.getItem("LoginMSG");
+        localStorage.setItem("Logged","True");
+    }
+  }
