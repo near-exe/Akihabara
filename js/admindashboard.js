ddf7d59d74fb45c6adf2542bd75ea498bc094563
Near <ogmelgen@gmail.com>
2023-04-10 17:54:35 -0400

    a

--- js/admindashboard.js
+++ js/admindashboard.js
@@ -5,7 +5,9 @@ fetch('/articulos')
     return response.json();
   })
   .then(function(data) {
-    
+    data.forEach(element => {
+      articulos.push(element);
+    });
 });
 //Coger la data que se manda desde el backend(un json con la tabla completa de compras vean la consola para que vean el output)
 fetch('/compras')
