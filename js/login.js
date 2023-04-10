ce4f7176b5026b419477dc72b47a63751363ff9e
Near <ogmelgen@gmail.com>
2023-04-09 22:52:27 -0400

    xd

--- js/login.js
+++ js/login.js
@@ -7,59 +7,6 @@ window.addEventListener("load", () => {
   });
 
 
-<<<<<<< HEAD
-  //Mensajes de feedback
-  const user = document.getElementById("user");
-  const password = document.getElementById("password");
-  const msg = document.getElementById("msg");
-
-  //sessionStorage.setItem("LoginMSG" , "");
-  msg.innerHTML = sessionStorage.getItem("LoginMSG");
-  localStorage.setItem("Logged", "False");
-
-
-  function Check()
-  {
-        //Traer la data de la ruta /logincheck que se manda desde el backend (Si se paso los checks del login)
-        fetch('/logincheck')
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(data) {
-          let flag = data;
-          if(flag)
-          {
-            return true;
-          }
-          else
-          {
-            return false;
-          }
-        });
-  }
-
-  function validateInput ()
-  {
-    let pass = password.value;
-    let user1 = user.value;
-    if(pass == "" || user1 == "")
-    {
-       sessionStorage.setItem("LoginMSG","Faltan completar campos");
-       msg.innerHTML = sessionStorage.getItem("LoginMSG");
-    }
-    else if(!Check())
-    {
-        sessionStorage.setItem("LoginMSG","Nombre de usuario o contraseña incorrectos");
-        msg.innerHTML = sessionStorage.getItem("LoginMSG");
-    }
-    else
-    {
-        sessionStorage.setItem("LoginMSG","");
-        msg.innerHTML = sessionStorage.getItem("LoginMSG");
-        localStorage.setItem("Logged","True");
-    }
-  }
-=======
 // después de que se verifique el inicio de sesión del usuario y se obtenga el nombre de usuario INTENTO 1
 // localStorage.setItem('username', nombreDeUsuario);
 
@@ -79,4 +26,3 @@ loginBtn.addEventListener('click', function() {
     
   }
 });
->>>>>>> niko
