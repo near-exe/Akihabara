b0128f2b05a34b14ef316dd4bc8aa40fd12f76bd
Angelleandro <ALJC0001@ce.pucmm.edu.do>
2023-04-10 16:36:54 -0400

    admin dashboard

--- js/admindashboard.js
+++ js/admindashboard.js
@@ -1,21 +1,21 @@
-let compras = [];
-let articulos = [];
+
 //Tomar la data de todos los articulos en la base de datos
 fetch('/articulos')
   .then(function(response) {
     return response.json();
   })
   .then(function(data) {
-    articulos=data;
+    data.forEach(element => {
+        
+    });
 });
-
 //Coger la data que se manda desde el backend(un json con la tabla completa de compras vean la consola para que vean el output)
 fetch('/compras')
   .then(function(response) {
     return response.json();
   })
   .then(function(data) {
-    compras=data;
+   
 });
 
 
